a1e1fd8059f6efe407c7cba2d29438b193954678
zivkidd <zeranxu@foxmail.com>
2022-07-17 23:08:11 +0800

    添加注释

--- do_adjust.py
+++ do_adjust.py
@@ -5,11 +5,13 @@ import numpy as np
 from scipy.sparse import lil_matrix
 import matplotlib.pyplot as plt
 
-#FILE_NAME = "problem-49-7776-pre.txt"
-FILE_NAME = "prob.txt"
+FILE_NAME = "problem-49-7776-pre.txt"
+# FILE_NAME = "prob.txt"
 
 def read_bal_data(file_name):
     with open(file_name, "rt") as file:
+
+        # 里面有49张影像 7776个点 共产生了31843次观测（从第i张影像观测到了第j个点则算一次）
         n_cameras, n_points, n_observations = map(
             int, file.readline().split())
 
@@ -17,17 +19,20 @@ def read_bal_data(file_name):
         point_indices = np.empty(n_observations, dtype=int)
         points_2d = np.empty((n_observations, 2))
 
+        # 读出观测的对应关系，从第camera_index张影像观测到了第point_index个点，其uv坐标为xy
         for i in range(n_observations):
             camera_index, point_index, x, y = file.readline().split()
             camera_indices[i] = int(camera_index)
             point_indices[i] = int(point_index)
             points_2d[i] = [float(x), float(y)]
 
+        # 每张影像有9个参数,3个旋转，3个平移，1个焦距，2个畸变
         camera_params = np.empty(n_cameras * 9)
         for i in range(n_cameras * 9):
             camera_params[i] = float(file.readline())
         camera_params = camera_params.reshape((n_cameras, -1))
 
+        # 每个点的xyz
         points_3d = np.empty(n_points * 3)
         for i in range(n_points * 3):
             points_3d[i] = float(file.readline())
@@ -78,8 +83,13 @@ def fun(params, n_cameras, n_points, camera_indices, point_indices, points_2d):
 
 
 def bundle_adjustment_sparsity(n_cameras, n_points, camera_indices, point_indices):
+    # 行，有多少个观测就有*2
     m = camera_indices.size * 2
+
+    # 列，有多少个参数就有，因为参数是个n*1的向量
     n = n_cameras * 9 + n_points * 3
+
+    # m*n的矩阵，n_cameras * 9列为相机参数，n_points * 3为三维点坐标
     A = lil_matrix((m, n), dtype=int)
 
     i = np.arange(camera_indices.size)
@@ -96,13 +106,19 @@ def bundle_adjustment_sparsity(n_cameras, n_points, camera_indices, point_indice
 def prettylist(l):
     return '[%s]' % ', '.join("%4.1e" % f for f in l)
 
+
+# 共31843个观测
+# camera_indices为31843*1，每个代表这次观测对应的像片索引
+# point_indices为31843*1，每个代表这次观测对应的三维点索引
+# points_2d为31843*2，每个代表这次观测对应的二维点坐标
 camera_params, points_3d, camera_indices, point_indices, points_2d = read_bal_data(FILE_NAME)
 
 # Print information
 n_cameras = camera_params.shape[0]
 n_points = points_3d.shape[0]
-
+# 这些都是参数，即相片的参数和三维点坐标
 n = 9 * n_cameras + 3 * n_points
+# 这些是残差，即重投影误差
 m = 2 * points_2d.shape[0]
 
 print("n_cameras: {}".format(n_cameras))
@@ -117,7 +133,7 @@ plt.figure(1)
 plt.subplot(211)
 plt.plot(f0)
 
-
+# A代表雅克比矩阵的哪些部分不为0
 A = bundle_adjustment_sparsity(n_cameras, n_points, camera_indices, point_indices)
 
 t0 = time.time()
